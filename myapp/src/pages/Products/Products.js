b5c6e3a16d2258aae3b79bb73c79d44fd3ba1a06
kamilsmr <kamils6035@gmail.com>
2022-01-06 00:57:45 +0300

    pages

---
+++ myapp/src/pages/Products/Products.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {safeAreaView, Text} from 'react-native';
+
+const Products = () => {
+  return (
+    <safeAreaView>
+      <Text>Products!</Text>
+    </safeAreaView>
+  );
+};
+
+export default Products;
