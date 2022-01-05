cb4f48ea2e4022dff29cff500f905c6308a43e27
kamilsmr <kamils6035@gmail.com>
2022-01-06 00:28:59 +0300

    start

---
+++ myapp/App.js
@@ -0,0 +1,16 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+
+const Section = ({children, title}): Node => {
+  const isDarkMode = useColorScheme() === 'dark';
+  return <View></View>;
+};
+
+export default App;
