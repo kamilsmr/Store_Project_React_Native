b934ade4d78c92150a5febcaff4bc5401fe86337
kamilsmr <kamils6035@gmail.com>
2022-01-11 01:11:33 +0300

    lottie animation

--- myapp/src/pages/Products/Products.js
+++ myapp/src/pages/Products/Products.js
@@ -1,24 +1,27 @@
 import React, {useState, useEffect} from 'react';
 import {safeAreaView, Text, FlatList, ActivityIndicator} from 'react-native';
 import Config from 'react-native-config';
-import axios from 'axios';
+import Error from '../../components/Error/Error';
+import Loading from '../../components/Loading/Loading';
+
 import ProductCard from '../../components/ProductCard';
 import useFetch from '../../hooks/useFetch';
 
 const Products = () => {
   const {loading, data, error} = useFetch(Config.API_URL);
-  console.log('render');
-  console.log({loading, data: data.length, error});
-  console.log('--------------');
+
+  // console.log('render');
+  // console.log({loading, data: data.length, error});
+  // console.log('--------------');
 
   const renderProduct = ({item}) => <ProductCard product={item} />;
 
   if (loading) {
-    return <ActivityIndicator size="large" />;
+    return <Loading />;
   }
 
   if (error) {
-    return <Text>{error}</Text>;
+    return <Error />;
   }
 
   return (
