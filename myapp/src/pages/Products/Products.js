df98aed18cd58b67b2f7586a637df77215e244ac
kamilsmr <kamils6035@gmail.com>
2022-01-11 00:42:04 +0300

    costum hook

--- myapp/src/pages/Products/Products.js
+++ myapp/src/pages/Products/Products.js
@@ -1,22 +1,26 @@
 import React, {useState, useEffect} from 'react';
-import {safeAreaView, Text, Flatlist} from 'react-native';
+import {safeAreaView, Text, FlatList, ActivityIndicator} from 'react-native';
 import Config from 'react-native-config';
 import axios from 'axios';
 import ProductCard from '../../components/ProductCard';
+import useFetch from '../../hooks/useFetch';
 
 const Products = () => {
-  const [data, setData] = useState([]);
-
-  useEffect(() => {
-    fetchData();
-  }, []);
-
-  const fetchData = async () => {
-    const {data: productData} = await axios.get(Config.API_URL);
-    setData(productData);
-  };
+  const {loading, data, error} = useFetch(Config.API_URL);
+  console.log('render');
+  console.log({loading, data: data.length, error});
+  console.log('--------------');
 
   const renderProduct = ({item}) => <ProductCard product={item} />;
+
+  if (loading) {
+    return <ActivityIndicator size="large" />;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
+
   return (
     <safeAreaView>
       <Flatlist data={data} renderItem={renderProduct} />
